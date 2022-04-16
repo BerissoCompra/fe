e0b5f3f0419e865ad3549d2c7ec485013457ee1f
DESKTOP-J1B1JIN\MCDsoftwares <berissocompratest@gmail.com>
2022-04-16 18:36:07 -0300

    Env cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
 
   urlAPI: 'http://ec2-44-201-231-118.compute-1.amazonaws.com:3000/api',
   beUrl: 'http://ec2-44-201-231-118.compute-1.amazonaws.com:3000/',
+
   production: true,
   firebase: {
     apiKey: "AIzaSyAl0OAwPbUHopfVYFKhXhg8IoKLFtCIXAA",
